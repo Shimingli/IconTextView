7e96fc6c3cba94ddc9b6247b670f9313cb5050f5
weiteng4droid <weiteng4droid@163.com>
2016-03-08 17:19:05 +0800

    Initial commit

---
+++ app/src/main/java/com/weiteng/demo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.weiteng.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.weiteng.widget.IconTextView;
+
+public class MainActivity extends AppCompatActivity implements IconTextView.OnIconTextClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IconTextView iconTextView1 = (IconTextView) findViewById(R.id.icon_tv);
+        IconTextView iconTextView2 = (IconTextView) findViewById(R.id.icon_tv2);
+        IconTextView iconTextView3 = (IconTextView) findViewById(R.id.icon_tv3);
+        IconTextView iconTextView4 = (IconTextView) findViewById(R.id.icon_tv4);
+
+        iconTextView1.setOnIconTextClickListener(this);
+        iconTextView2.setOnIconTextClickListener(this);
+        iconTextView3.setOnIconTextClickListener(this);
+        iconTextView4.setOnIconTextClickListener(this);
+    }
+
+    @Override
+    public void onIconTextClick(IconTextView view) {
+        String text = view.getText();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+}
